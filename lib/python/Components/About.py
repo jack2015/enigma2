77b71f56656616b3bb13a9def46d32c9e0af870a
jack2015 <344784320@qq.com>
2020-07-11 17:28:06 +0800

    update

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -3,6 +3,18 @@ from sys import modules
 from os import path
 import socket, fcntl, struct
 
+def getFlashMemory(folder='/'):
+	try:
+		from os import statvfs
+		diskSpace = os.statvfs(folder)
+		available = float(diskSpace.f_bsize * diskSpace.f_bavail)
+		fspace=round(float((available) / (1024.0*1024.0)),2)
+		spacestr=str(fspace)+'M'
+		return spacestr
+	except:
+		pass
+	return _("unavailable")
+
 def getVersionString():
 	return getImageVersion()
 
