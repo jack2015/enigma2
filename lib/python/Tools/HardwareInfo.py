a272204ac973cd5eded6f6cedb7a7141a084b6e7
jack2015 <344784320@qq.com>
2021-12-01 22:42:33 +0800

    update

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -1,4 +1,4 @@
-from boxbranding import getBoxType, getBrandOEM
+from boxbranding import getBoxType, getBrandOEM, getMachineName
 from Components.About import about
 
 
@@ -16,6 +16,8 @@ class HardwareInfo:
 			file = open("/proc/stb/info/model", "r")
 			HardwareInfo.device_name = file.readline().strip()
 			file.close()
+			if getBrandOEM() == "dags":
+				HardwareInfo.device_name = "dm800se"
 			try:
 				file = open("/proc/stb/info/version", "r")
 				HardwareInfo.device_version = file.readline().strip()
@@ -35,6 +37,9 @@ class HardwareInfo:
 				elif "Brcm7401 V0.0" in rd:
 					HardwareInfo.device_name = "dm800"
 					print "dm800 detected!"
+				elif "MIPS 4KEc V4.8" in rd:
+					HardwareInfo.device_name = "dm7025"
+					print "dm7025 detected!"
 			except:
 				pass
 
@@ -44,13 +49,28 @@ class HardwareInfo:
 	def get_device_version(self):
 		return HardwareInfo.device_version
 
+	def get_device_model(self):
+		return getBoxType()
+
+	def get_vu_device_name(self):
+		return getBoxType()
+
+	def get_friendly_name(self):
+		return getMachineName()
+
 	def has_hdmi(self):
-		return getBrandOEM() in ('vuplus') and HardwareInfo.device_version is not None
+		return not (HardwareInfo.device_name == 'dm800' or (HardwareInfo.device_name == 'dm8000' and HardwareInfo.device_version == None))
+
+	def linux_kernel(self):
+		try:
+			return open("/proc/version", "r").read().split(' ', 4)[2].split('-', 2)[0]
+		except:
+			return "unknown"
 
 	def has_deepstandby(self):
 		return getBoxType() != 'dm800'
 
 	def is_nextgen(self):
-		if about.getCpuCoresInt() < 2 or about.getCPUSpeedMHzInt() < 750:
-			return False
-		return True
+		if about.getCPUString() in ('BCM7346B2', 'BCM7425B2', 'BCM7429B0'):
+			return True
+		return False
